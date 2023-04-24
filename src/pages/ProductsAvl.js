694abe3d34a1adc769d7c831d61ee2030f4ce82c
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-24 07:17:16 +0200

    modify 1

--- src/pages/ProductsAvl.js
+++ src/pages/ProductsAvl.js
@@ -7,14 +7,15 @@ export const ProductAvl =()=>{
     const products = ["TomATO","potato","fish","cocoa","tatana"]
     return (
         
-        
-        <div className={styles.background}>
+<div className={styles.background}>
+<div className={styles.overlay}>
              <video src={background} autoPlay loop muted/> 
         <div className={styles.word}>
            
             {products.map((food)=>{ return (<h1>{food}</h1>)})}
             <iframe width="560" height="315" src="https://www.youtube.com/embed/bjQqIe6nM3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <h1 >Welcome to my website</h1>
+        </div>
         </div>
         </div>
     )
