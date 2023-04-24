935ced00e291a30dee83b82d68ce7e6f1ab5d391
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-24 06:58:22 +0200

    adding a video as a background

--- src/pages/ProductsAvl.js
+++ src/pages/ProductsAvl.js
@@ -1,9 +1,21 @@
+import background from './video/background.mp4'
+import styles from '../App.module.css'
+
+
+
 export const ProductAvl =()=>{
-    const products = ["TomATO","potato","fish","cocoa"]
+    const products = ["TomATO","potato","fish","cocoa","tatana"]
     return (
-        <div>
-            {products.map((food)=>{ return (<h1>{food}</h1>)
-            })}
+        
+        
+        <div className={styles.background}>
+             <video src={background} autoPlay loop muted/> 
+        <div className={styles.word}>
+           
+            {products.map((food)=>{ return (<h1>{food}</h1>)})}
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/bjQqIe6nM3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h1 >Welcome to my website</h1>
+        </div>
         </div>
     )
 }
